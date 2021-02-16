930da04bc64366efc1b469805812c8814857b1a6
Harry Chen <harrychen0314@gmail.com>
2021-02-15 20:32:28 -0800

    fix problem 5 solution

--- 5/solution.cpp
+++ 5/solution.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 int main()
 {
-    cout<<"  _____     ____  "<<endl;
-    cout<<" |_ \" _| U /\"___| "<<endl;
-    cout<<"   | |   \\| | u"<<endl;
-    cout<<"  /| |\\   | |/__"<<endl;
-    cout<<" u |_|U    \\____|"<<endl;
-    cout<<" _// \\_  _// \\"<<endl;
-    cout<<"(__) (__)(__)(__) "<<endl;
+    cout << "  _____     ____  " << endl;
+    cout << " |_ \" _| U /\"___| " << endl;
+    cout << "   | |   \\| | u" << endl;
+    cout << "  /| |\\   | |/__" << endl;
+    cout << " u |_|U    \\____|" << endl;
+    cout << " _// \\\\_  _// \\\\" << endl;
+    cout << "(__) (__)(__)(__) " << endl;
+    return 0;
 }

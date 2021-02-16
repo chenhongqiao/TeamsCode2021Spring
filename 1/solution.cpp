977895df5eba1e4e69e311e259f738bf62f33276
Harry Chen <harrychen0314@gmail.com>
2021-02-15 20:23:26 -0800

    add solution to problem 1

---
+++ 1/solution.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<<"  _____     ____  "<<endl;
+    cout<<" |_ \" _| U /\"___| "<<endl;
+    cout<<"   | |   \\| | u"<<endl;
+    cout<<"  /| |\\   | |/__"<<endl;
+    cout<<" u |_|U    \\____|"<<endl;
+    cout<<" _// \\_  _// \\"<<endl;
+    cout<<"(__) (__)(__)(__) "<<endl;
+}

35cd8753999b863551e420b4d09cad2ba79a0570
Harry Chen <harrychen0314@gmail.com>
2021-02-22 18:37:46 -0800

    update problem 10 solution

--- 10/solution.cpp
+++ 10/solution.cpp
@@ -6,8 +6,8 @@ int main()
     cin >> a;
     int s, d;
     cin >> s >> d;
-    double t = stoi(a.substr(0, 2).c_str()) * 60 + stoi(a.substr(3).c_str());
-    t += 1.0 * d / s * 60;
-    cout << setfill('0') << setw(2) << ((int)t / 60) % 24 << ":" << setfill('0') << setw(2) << (int)t % 60 << endl;
+    int t = stoi(a.substr(0, 2).c_str()) * 60 + stoi(a.substr(3).c_str());
+    t += 60 * d / s;
+    cout << setfill('0') << setw(2) << (t / 60) % 24 << ":" << setfill('0') << setw(2) << t % 60 << endl;
     return 0;
 }

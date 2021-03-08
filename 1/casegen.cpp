dbb27a5bf2355ac6e8b74c0e140a56eb3c4698c5
Harry Chen <harrychen0314@gmail.com>
2021-03-07 20:03:09 -0800

    add problem 1 testcases

---
+++ 1/casegen.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define rand(l, h) (rand() % (h - l + 1) + l)
+using namespace std;
+int main()
+{
+    srand(time(0));
+    int n;
+    cin >> n;
+    cout << rand(0, n) << " " << rand(0, n) << endl;
+}

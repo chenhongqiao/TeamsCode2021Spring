df208b300f011b86ee009bfe189fefe3a83e0862
Harry Chen <harrychen0314@gmail.com>
2021-03-10 19:53:18 -0800

    add problem 13 solution

---
+++ 13/solution.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define umap unordered_map
+using namespace std;
+umap<long long, int> feq;
+vector<long long> c;
+int main()
+{
+    int n;
+    long long m;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        int v;
+        cin >> v;
+        c.push_back(v);
+        feq[v]++;
+    }
+    long long ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        ans += feq[m - i];
+    }
+    cout << ans << endl;
+    return 0;
+}

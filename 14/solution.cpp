15dfaa80172fdcb5afcb64d26be3851a1f3ed1c3
Harry Chen <harrychen0314@gmail.com>
2021-03-17 16:59:21 -0700

    add solution to problem 14

---
+++ 14/solution.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long n;
+    cin >> n;
+    const int nrt = sqrt(n);
+    for (int i = 2; i <= nrt + 1; i++)
+    {
+        int cnt = 0;
+        while (n > 0 && n % i == 0)
+        {
+            n /= i;
+            cnt++;
+        }
+        if (cnt != 0)
+        {
+            cout << i << " " << cnt << endl;
+        }
+    }
+    return 0;
+}

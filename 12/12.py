fd4b915fc3cb19cddeb60f1df432386a0d6bcbd3
Sol Park <43934310+sollymolly@users.noreply.github.com>
2021-03-17 19:18:45 -0700

    Add files via upload

---
+++ 12/12.py
@@ -0,0 +1,27 @@
+N, num_blocks, radius = map(int, input().split())
+ar = list(map(int, input().split()))
+
+ar = sorted(ar)
+
+res = 0
+
+if ar[0] - radius > 0:
+    res += 1
+
+for i in range(N):
+    if ar[i] + radius > num_blocks - 1:
+        break
+
+    if i == N - 1:
+        if (ar[i] + radius <= num_blocks - 1):
+            res += 1
+        break
+    else:
+        if (ar[i] + radius < ar[i+1] - radius):
+            res += 1
+            
+print(res)
+
+
+
+

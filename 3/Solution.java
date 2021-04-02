038174f28cc5ddc5f17dfb90bf4e6f4c634d9f53
Harry Chen <harrychen0314@gmail.com>
2021-04-02 11:39:00 -0700

    update problem 3 java solution

--- 3/Solution.java
+++ 3/Solution.java
@@ -1,32 +1,19 @@
-// This solution needs to be updated
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
-public class UnitConversion
-{
-    public static void main(String[] args)
-    {
+public class Solution {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int M = sc.nextInt();
-        Map<String, Double> factorMap = new HashMap<>();
-        for (int i = 0; i < N; i++)
-        {
-            double u1Amt = sc.nextDouble();
-            String u1Name = sc.next();
-            double u2Amt = sc.nextDouble();
-            String u2Name = sc.next();
-            factorMap.put(u1Name + " " + u2Name, u2Amt / u1Amt);
-            factorMap.put(u2Name + " " + u1Name, u1Amt / u2Amt);
+        for (int i = 0; i < N; i++) {
+            char op = sc.next().charAt(0);
+            int A = sc.nextInt();
+            if (op == '+') {
+                M += A;
+            } else {
+                M -= A;
+            }
         }
-        for (int i = 0; i < M; i++)
-        {
-            double baseAmt = sc.nextDouble();
-            String baseUnit = sc.next();
-            String tgtUnit = sc.next();
-            double target = factorMap.get(baseUnit + " " + tgtUnit) * baseAmt;
-            System.out.printf("%.3f\n", target);
-        }
+        System.out.println(M);
     }
 }

a57366b13dc88c4ef787f85f12506c51186c1e55
Sol Park <43934310+sollymolly@users.noreply.github.com>
2021-04-21 19:11:52 -0700

    Update 15.py

--- 15/15.py
+++ 15/15.py
@@ -1,29 +1,37 @@
-# problem updated
-from collections import deque
-
 N, M = map(int, input().split())
+maze_dist = []
+maze_num = []
+maze_vis = []
+
+for _ in range(N):
+    maze_dist.append([0]*M)
+    maze_num.append([0]*M)
+    maze_vis.append([0]*M)
+
 maze = []
-done = False
 
 for _ in range(N):
     maze.append(list(map(int, input().split())))
 
-if maze[0][0] or maze[N-1][0]:
-    print(-1)
-
-queue = [(0, 0, 1)]
-maze[0][0] = 1
+queue = [(0, 0, 0)]
+maze_vis[0][0] = 1
+maze_num[0][0] = 1
 
 for i, j, d in queue:
-    if i == N-1 and j == 0:
-        print(d-1)
-        done = True
-        break
 
-    for x, y in ((i-1, j), (i, j-1), (i, j+1), (i+1, j)):
-        if 0 <= x < N and 0 <= y < N and not maze[x][y]:
-            maze[x][y] = 1
-            queue.append((x, y, d+1))
+    for x, y in ((i, j+1), (i+1, j), (i-1, j), (i, j-1)):
 
-if not done:
-    print(-1)
+        if 0 <= x < N and 0 <= y < M and maze[x][y] == 0:
+            if maze_vis[x][y] == 0:
+                maze_dist[x][y] = d
+                maze_num[x][y] = maze_num[i][j]
+                maze_vis[x][y] = 1
+                queue.append((x, y, d+1))
+            elif d == maze_dist[x][y]:
+                maze_num[x][y] += maze_num[i][j] 
+                maze_num[x][y] = maze_num[x][y] % (10**9+7)
+        
+print(maze_num[N-1][M-1])
+
+        
+

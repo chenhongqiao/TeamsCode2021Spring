885bbe91dc92c1f5d9a0d0f9374cfda0d6590ec8
Alex <alexdu9876@gmail.com>
2021-04-22 14:15:13 -0700

    Seems to work

--- 24/solution.cpp
+++ 24/solution.cpp
@@ -23,6 +23,10 @@ vector <int> edge[maxn];
 int dfn[maxn], cnt, low[maxn];
 stack <int> st;
 int g[maxn], gcnt;
+vector <int> de[maxn];
+int deg[maxn], sz[maxn], cur[maxn];
+set <pii> he;
+ll ans = 0;
 
 void tarjan(int u) {
     dfn[u] = ++cnt;
@@ -31,7 +35,24 @@ void tarjan(int u) {
     for (int v : edge[u]) {
         if (!dfn[v]) {
             tarjan(v);
-            low[u] = min(low
+            low[u] = min(low[u], low[v]);
+        }
+        else low[u] = min(low[u], dfn[v]);
+    }
+    if (dfn[u] == low[u]) {
+        ++gcnt;
+        while (st.top() != u) {
+            g[st.top()] = gcnt;
+            ++sz[gcnt];
+            st.pop();
+        }
+        g[st.top()] = gcnt;
+        ++sz[gcnt];
+        cur[gcnt] = sz[gcnt];
+        ans += sz[gcnt] * (sz[gcnt] - 1) / 2;
+        st.pop();
+    }
+}
 
 int main() {
     scanf("%d%d", &n, &m);
@@ -43,5 +64,29 @@ int main() {
     for (int i = 1; i <= n; ++i) {
         if (!dfn[i]) tarjan(i);
     }
+    for (int u = 1; u <= n; ++u) {
+        for (int v : edge[u]) {
+            if (g[u] != g[v] && he.find(mp(g[u], g[v])) == he.end()) {
+                he.insert(mp(g[u], g[v]));
+                de[g[u]].pb(g[v]);
+                ++deg[g[v]];
+            }
+        }
+    }
+    queue <int> q;
+    for (int i = 1; i <= n; ++i) {
+        if (!deg[i]) q.push(i);
+    }
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        for (int v : de[u]) {
+            ans += cur[u] * sz[v];
+            cur[v] += cur[u];
+            --deg[v];
+            if (!deg[v]) q.push(v);
+        }
+    }
+    printf("%lld", ans);
 }
 

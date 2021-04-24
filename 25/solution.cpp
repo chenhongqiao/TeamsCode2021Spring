669c25fd0338e890d39fb4e9181a31873f84f5af
Harry Chen <harrychen0314@gmail.com>
2021-04-24 00:42:07 -0700

    problem 25 cases

--- 25/solution.cpp
+++ 25/solution.cpp
@@ -9,27 +9,29 @@
 #include <map>
 #include <list>
 using namespace std;
-typedef pair <int, int> pii;
+typedef pair<int, int> pii;
 typedef long long ll;
-#define pb push_back 
+#define pb push_back
 #define mp make_pair
 #define f first
 #define s second
 
-const int maxn = 5e4 + 10, inf = 1e9;
+const int maxn = 5e6 + 10, inf = 1e9;
 
-int n, m;//number of tasks, number of rewards
+int n, m; //number of tasks, number of rewards
 int c[maxn], r[maxn];
 
 //first m nodes are rewards, next n nodes are tasks, n + m + 1 is supersource, n + m + 2 is supersink
 int s, t;
 
-struct edge{
+struct edge
+{
     int to, nxt, cp;
-}p[5000020];
+} p[5000020];
 int ohead[maxn], head[maxn], ecnt = 2;
 
-void addedge(int u, int v, int c) {
+void addedge(int u, int v, int c)
+{
     p[ecnt].nxt = ohead[u];
     p[ecnt].to = v;
     p[ecnt].cp = c;
@@ -42,19 +44,24 @@ void addedge(int u, int v, int c) {
 
 int dep[maxn];
 
-bool set_dep() {
+bool set_dep()
+{
     memset(dep, 0, sizeof(dep));
     dep[s] = 1;
-    queue <int> q;
+    queue<int> q;
     q.push(s);
-    while (!q.empty()) {
+    while (!q.empty())
+    {
         int u = q.front();
         q.pop();
         head[u] = ohead[u];
-        if (u == t) return true;
-        for (int i = head[u]; i; i = p[i].nxt) {
+        if (u == t)
+            return true;
+        for (int i = head[u]; i; i = p[i].nxt)
+        {
             int v = p[i].to;
-            if (dep[v] || !p[i].cp) continue;
+            if (dep[v] || !p[i].cp)
+                continue;
             dep[v] = dep[u] + 1;
             q.push(v);
         }
@@ -62,50 +69,61 @@ bool set_dep() {
     return false;
 }
 
-ll aug(int u, ll f) {
-    if (u == t) return f;
+ll aug(int u, ll f)
+{
+    if (u == t)
+        return f;
     ll ans = 0;
-    for (int i = head[u]; i; i = p[i].nxt) {
+    for (int i = head[u]; i; i = p[i].nxt)
+    {
         int v = p[i].to;
-        if (dep[v] == dep[u] + 1) {
+        if (dep[v] == dep[u] + 1)
+        {
             ll nf = aug(v, min((ll)p[i].cp, f));
             ans += nf;
             p[i].cp -= nf;
             p[i ^ 1].cp += nf;
             f -= nf;
         }
-        if (f) head[u] = i;
-        else break;
+        if (f)
+            head[u] = i;
+        else
+            break;
     }
     return ans;
 }
 
-int main() {
+int main()
+{
     scanf("%d%d", &n, &m);
     s = n + m + 1, t = n + m + 2;
-    for (int i = 1; i <= n; ++i) {
+    for (int i = 1; i <= n; ++i)
+    {
         scanf("%d", &c[i]);
         addedge(m + i, t, c[i]);
     }
     ll ans = 0;
-    for (int i = 1; i <= m; ++i) {
+    for (int i = 1; i <= m; ++i)
+    {
         scanf("%d", &r[i]);
         ans += r[i];
         addedge(s, i, r[i]);
     }
-    for (int i = 1; i <= m; ++i) {
-        int x; 
+    for (int i = 1; i <= m; ++i)
+    {
+        int x;
         scanf("%d", &x);
         int tsk;
-        for (int j = 0; j < x; ++j) {
+        for (int j = 0; j < x; ++j)
+        {
             scanf("%d", &tsk);
             addedge(i, m + tsk, inf);
         }
     }
     ll minc = 0;
-    while (set_dep()) {
+    while (set_dep())
+    {
         minc += aug(s, 1e15 + 100);
     }
     printf("%lld", ans - minc);
 }
-

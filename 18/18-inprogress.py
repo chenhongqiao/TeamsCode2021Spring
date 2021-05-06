d256020c5c8519bd052056f80c94a5490030d000
Sol Park <43934310+sollymolly@users.noreply.github.com>
2021-05-05 17:09:56 -0700

    Update 18-inprogress.py

--- 18/18-inprogress.py
+++ 18/18-inprogress.py
@@ -1,50 +1,49 @@
-#include <bits/stdc++.h>
-from queue import PriorityQueue
-
-f, sumnums, tree = [], [], []
-
-for _ in range(100005):
-    f.append(0)
-    sumnums.append(0)
-    tree.append(0)
-    
-q = PriorityQueue() #priority_queue<pair<int, int>> q;
-
-def summation(p):
+def summation(p): #long long sum(int p)
     s = 0
     while (p > 0):
         s += tree[p]
         p -= p and -p
-    
+
     return s
 
-def update(p, v):
+def update(p, v): #void update(int p, long long v)
     while (p <= n + 1):
         tree[p] += v
         p += p and -p
-        
 
+priority_queue<pair<int, int>> q; #python translation
+
+a, f, snum, tree = [0], [], [], []
 N = int(input())
-a = list(map(int, input().split()))
 
-for i in range(N-1):
+for _ in range(100005):
+    f.append(0)
+    snum.append(0)
+    tree.append(0)
+
+for i in range(1, N+1): #for (int i = 1; i <= n; i++) why?
+    a.append(int(input()))
+
+for j in range(N-1): #for (int i = 0; i < n - 1; i++)
     a, b = map(int, input().split())
-    sumnums[a] += 1
-    f[b] = a #check
+    snum[a] += 1
+    f[b] = a
 
-for j in range(1, N+1):
-    if (sumnums[i] == 0):
-        q.put([a[i], i])
+for k in range(1, N+1): #for (int i = 1; i <= n; i++)
+    if (snum[k] == 0):
+        q.push({a[i], i}) #python translation?
 
 res = 0
-while (q):
-    p = q.top()[1] #python3 equivalent???
-    res += sum(p)
-    res += (q.top()[0]) * 3 #python3 equivalent???
-    update(p, q.top()[0]) #python3 equivalent
-    q.pop()
-    sumnums[f[p]] -= 1
-    if (sumnums[f[p]] == 0):
-        q.put([a[f[p]], f[p]])
 
+while (q):
+    p = q.top()[1] #python translation
+    res += sum(p)
+    res += (q.top()[0]) * 3 #python translation
+    update(p, q.top()[0]) #python translation
+    q.pop() #python translation
+    snum[f[p]] -= 1
+
+    if (snum[f[p]] == 0):
+        q.push([a[f[p]], f[p]]) #python translation
+    
 print(res)

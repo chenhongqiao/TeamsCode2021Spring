563d33230d5183315c8871cc2ad1d0f6cb1ffbf3
Sol Park <43934310+sollymolly@users.noreply.github.com>
2021-05-08 03:05:28 -0700

    Update 17-inprogress.py

--- 17/17-inprogress.py
+++ 17/17-inprogress.py
@@ -1,43 +1,45 @@
+N, M = map(int, input().split())
+res = 0
+st = []
+g, v = {}, {}
+for id in range(N+1):
+    g[id] = {}
+    v[id] = {}
+    for j in range(N+1):
+        g[id][j] = []
+        v[id][j] = False
+
 def bfs(x, y):
     q = []
     q.append([x, y])
     cnt = 1
 
     while (q):
-        kx = q[0][0]
-        ky = q[0][1]
-        for i in range(len(g[kx][ky])):
-            nx = g[kx][ky][i][0]
-            ny = g[kx][ky][i][1]
+        kx, ky = q[0][1], q[0][1]
+
+        for idx in range(len(g[kx][ky])): #i < g[kx][ky].size(); i++)
+            nx, ny = g[kx][ky][idx][0], g[kx][ky][idx][1]
+        
             if (not v[nx][ny]):
                 v[nx][ny] = True
                 q.append([nx, ny])
                 cnt += 1
+    
+        q.pop(0)
+    
+    return (cnt) * (cnt - 1)
 
-        q.pop()
-
-    return (cnt * (cnt - 1))
-
-N, M = map(int, input().split())
-res = 0
-st = [] #vector<pair<int, int>> st;
-umap<int, umap<int, vector<pair<int, int>>>> g; #this
-umap<int, umap<int, bool>> v; #this
-
-for i in range(M):
-    x1, y1, x2, y2 = map(int, input().split())
+for _ in range(M):
+    x1, x2, y1, y2 = map(int, input().split())
     g[x1][y1].append([x2, y2])
     g[x2][y2].append([x1, y1])
     st.append([x1, y1])
     st.append([x2, y2])
 
-res = n * n
-for j in range(len(st)):
-    if (not v[st[i][0]][st[i][1]])
-    {
+res = N*N
+for i in range(len(st)):
+    if (not v[st[i][0]][st[i][1]]):
         v[st[i][0]][st[i][1]] = True
         res += bfs(st[i][0], st[i][1])
-    }
 
 print(res)
-

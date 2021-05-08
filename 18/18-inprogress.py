aa84bf39894ed316bdc05b27a685b09319ede6c0
Sol Park <43934310+sollymolly@users.noreply.github.com>
2021-05-08 03:10:37 -0700

    Update 18-inprogress.py

--- 18/18-inprogress.py
+++ 18/18-inprogress.py
@@ -2,16 +2,16 @@ def summation(p): #long long sum(int p)
     s = 0
     while (p > 0):
         s += tree[p]
-        p -= p and -p
+        p -= p & (-p)
 
     return s
 
-def update(p, v): #void update(int p, long long v)
-    while (p <= n + 1):
+def update_queue(p, v): #void update(int p, long long v)
+    while (p <= N + 1):
         tree[p] += v
-        p += p and -p
+        p += p & (-p)
 
-priority_queue<pair<int, int>> q; #python translation
+q = [] #python translation
 
 a, f, snum, tree = [0], [], [], []
 N = int(input())
@@ -21,29 +21,33 @@ for _ in range(100005):
     snum.append(0)
     tree.append(0)
 
-for i in range(1, N+1): #for (int i = 1; i <= n; i++) why?
-    a.append(int(input()))
+arr = list(map(int, input().split()))
+for num in arr:
+    a.append(num)
 
 for j in range(N-1): #for (int i = 0; i < n - 1; i++)
-    a, b = map(int, input().split())
-    snum[a] += 1
-    f[b] = a
+    a_num, b = map(int, input().split())
+    snum[a_num] += 1
+    f[b] = a_num
 
 for k in range(1, N+1): #for (int i = 1; i <= n; i++)
     if (snum[k] == 0):
-        q.push({a[i], i}) #python translation?
+        q.append([a[k], k]) #python translation?
+        q = sorted(q, key = lambda x: -x[0])
 
 res = 0
 
 while (q):
-    p = q.top()[1] #python translation
-    res += sum(p)
-    res += (q.top()[0]) * 3 #python translation
-    update(p, q.top()[0]) #python translation
-    q.pop() #python translation
+    top = q[0]
+    p = top[1] #python translation
+    res += summation(p)
+    res += (top[0]) * 3 #python translation
+    update_queue(p, top[0]) #python translation
+    q.pop(0) #python translation
     snum[f[p]] -= 1
 
     if (snum[f[p]] == 0):
-        q.push([a[f[p]], f[p]]) #python translation
+        q.append([a[f[p]], f[p]]) #python translation
+        q = sorted(q, key = lambda x: -x[0])
     
 print(res)

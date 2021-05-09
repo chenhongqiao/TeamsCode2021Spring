709a835f1d579018a468d7be99b669167efa8e07
Sol Park <43934310+sollymolly@users.noreply.github.com>
2021-05-09 11:36:22 -0700

    Update 17-inprogress.py

--- 17/17-inprogress.py
+++ 17/17-inprogress.py
@@ -13,33 +13,35 @@ def bfs(x, y):
     q = []
     q.append([x, y])
     cnt = 1
-
-    while (q):
+    while q:
         kx, ky = q[0][0], q[0][1]
 
-        for idx in range(len(g[kx][ky])):
-            nx, ny = g[kx][ky][idx][0], g[kx][ky][idx][1]
-        
+        for i in range(len(g[kx][ky])): #(int i = 0; i < g[kx][ky].size(); i++)
+            nx, ny = g[kx][ky][i][0], g[kx][ky][i][1]
+
             if (not v[nx][ny]):
                 v[nx][ny] = True
                 q.append([nx, ny])
                 cnt += 1
-    
+            
         q.pop(0)
-    
-    return (cnt) * (cnt - 1)
 
-for _ in range(M):
-    x1, x2, y1, y2 = map(int, input().split())
+    return cnt * (cnt - 1) #return ((long long)cnt) * (cnt - 1);
+
+for _ in range(M): #(int i = 0; i < m; i++)
+    x1, y1, x2, y2 = map(int, input().split())
     g[x1][y1].append([x2, y2])
     g[x2][y2].append([x1, y1])
     st.append([x1, y1])
     st.append([x2, y2])
 
-res = N*N
-for i in range(len(st)):
-    if (not v[st[i][0]][st[i][1]]):
-        v[st[i][0]][st[i][1]] = True
-        res += bfs(st[i][0], st[i][1])
+res = N * N
+
+for j in range(len(st)): #(int i = 0; i < st.size(); i++)
+
+    if (not v[st[j][0]][st[j][1]]):
+        v[st[j][0]][st[j][1]] = True
+        res += bfs(st[j][0], st[j][1])
 
 print(res)
+
